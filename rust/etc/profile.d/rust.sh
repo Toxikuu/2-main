48378cde597fd5b9760cecf8f51b7170e91850e2
Toxikuu <140706894+Toxikuu@users.noreply.github.com>
2025-02-13 20:52:10 -0600

    Moved main/rust to tox/rust

--- rust/etc/profile.d/rust.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Begin /etc/profile.d/rust.sh
-
-export CARGO_HOME="/opt/cargo"
-export RUSTUP_HOME="/opt/rustup"
-pathprepend "\$CARGO_HOME/bin"
-
-# End /etc/profile.d/rust.sh
